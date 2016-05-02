c96003fccbbbffe47051732bd0ecaf14279d2302
Antis Smith <Antis Smith>
2016-05-02 23:01:47 +0300

    Скрипт для поднятия крыльев из сундука.

---
+++ Assets/1_Scripts/Bonus_Items/WingUp.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WingUp : MonoBehaviour
+{
+    const string NAME_CLASS = "WingsItems";
+
+
+    void OnTriggerEnter2D( Collider2D other )
+    {
+        print( "OnTriggerEnter2D" );
+        Check( other.gameObject );        
+    }
+
+    void OnCollisionEnter2D( Collision2D coll )
+    {
+        print( "OnCollisionEnter2D" );
+        Check( coll.gameObject );        
+    }
+
+    void Check(GameObject go)
+    {
+        print( "000" );
+        if( go.CompareTag( "Player" ) )
+        {
+            print( "1!!!!" );
+            var wing = PoolReference.items_D[NAME_CLASS] as WingsItems;
+            wing.AddItem();
+            Destroy( gameObject );
+        }
+    }
+}
