326a45453701964f50b5beabab304235635d76d9
Antis Smith <Antis Smith>
2016-05-02 23:04:23 +0300

    Убрал генерацию по клику левой кнопки мыши.

--- Assets/1_Scripts/ProceduralCaveGeneration/MapGenerator.cs
+++ Assets/1_Scripts/ProceduralCaveGeneration/MapGenerator.cs
@@ -27,9 +27,9 @@ public class MapGenerator : MonoBehaviour
 
     void Update()
     {
-        if( Input.GetMouseButtonDown( 0 ) )
+       // if( Input.GetMouseButtonDown( 0 ) )
         {
-            GenerateMap();
+           // GenerateMap();
         }
     }
 
