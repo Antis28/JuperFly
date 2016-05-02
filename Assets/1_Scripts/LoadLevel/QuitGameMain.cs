0080b05dd1092c1c7bfe25aa415d666f114f7720
Antis Smith <Antis Smith>
2016-05-02 23:06:06 +0300

    Добавил возможность начать игру заново, сохранив при этом копию.

--- Assets/1_Scripts/LoadLevel/QuitGameMain.cs
+++ Assets/1_Scripts/LoadLevel/QuitGameMain.cs
@@ -1,21 +1,46 @@
 ﻿using UnityEngine;
-using System.Collections;
+using UnityEngine.SceneManagement;
+using System.IO;
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
 public class QuitGameMain : MonoBehaviour
 { //MainMenu
 
+    string pathToSave = "Data/Save";
+
     public void NewGame()
-	{
-		Application.LoadLevel (1);
-	}
-	public void QuitGame()
-	{
-		#if UNITY_EDITOR
-		EditorApplication.isPlaying = false;
-		#else
+    {
+        int count = 1;
+        //Application.LoadLevel (1);        
+        if( Directory.Exists( pathToSave ) )
+        {
+            print( "Delete" );
+
+            string newName = pathToSave + count;
+
+            while( Directory.Exists( newName ) )
+            {
+                newName = pathToSave + count++;
+            }
+            Directory.Move( pathToSave, newName );
+            Directory.CreateDirectory( pathToSave );
+        }
+        SceneManager.LoadScene( 1 );
+
+    }
+
+    public void ContinueGame()
+    {
+        SceneManager.LoadScene( 1 );
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+#else
 		Application.Quit();
-		#endif
-	}
+#endif
+    }
 }
