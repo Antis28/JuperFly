56b9f9a4adb2e3191884e7f9412e97874a6f6896
Antis Smith <Antis Smith>
2016-05-03 01:14:19 +0300

    Resurses

---
+++ !_ScriptsTemp/Editor/MyWindow.cs
@@ -0,0 +1,38 @@
+﻿//C# Example
+using UnityEditor;
+using UnityEngine;
+
+public class MyWindow : EditorWindow
+{
+/*	string myString = "Hello World";
+	bool groupEnabled = true;
+	bool groupEnabled_1 = false;
+	bool myBool = true;
+	float myFloat = 1.23f;
+	
+	// Add menu item named "My Window" to the Window menu
+	[MenuItem("Window/My Window")]
+	public static void ShowWindow()
+	{
+		//Show existing window instance. If one doesn't exist, make one.
+		EditorWindow.GetWindow(typeof(MyWindow));
+	}
+	
+	void OnGUI()
+	{
+		GUILayout.Label ("Base Settings", EditorStyles.boldLabel);
+
+		myString = EditorGUILayout.TextField ("Text Field", myString);
+
+		GUILayout.Label (myString, EditorStyles.miniLabel);
+		
+		groupEnabled = EditorGUILayout.BeginToggleGroup ("Optional Settings", groupEnabled);
+		myBool = EditorGUILayout.Toggle ("Toggle", myBool);
+		myFloat = EditorGUILayout.Slider ("Slider", myFloat, -3, 3);
+		EditorGUILayout.EndToggleGroup ();
+
+		groupEnabled_1 = EditorGUILayout.BeginToggleGroup ("Optional Ses", groupEnabled_1);
+		EditorGUILayout.EndToggleGroup ();
+	}
+*/
+}
