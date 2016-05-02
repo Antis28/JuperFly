fdfaa44235bcf0e1349cfda6deae02b466cd910a
Antis Smith <Antis Smith>
2016-05-02 23:05:14 +0300

    Добавил переход не по координатам, а по объекту.

--- Assets/1_Scripts/Triggers/Portal.cs
+++ Assets/1_Scripts/Triggers/Portal.cs
@@ -3,13 +3,21 @@ using System.Collections;
 
 public class Portal : MonoBehaviour {
 
-	public Vector3 vec3 = new Vector3 (211f, 87f, 0f); 
+	public Vector3 vec3 = new Vector3 (211f, 87f, 0f);
+    public Transform exitPortal;
+    public GameObject destroyCave;
 
-	void OnTriggerEnter2D(Collider2D other)
+    void OnTriggerEnter2D(Collider2D other)
 	{
 		
 		if (other.gameObject.CompareTag ("Player") || other.name == "hero") 
 		{
+            if( exitPortal != null )
+            {
+                other.gameObject.GetComponent<RectTransform>().position = exitPortal.position;
+                Destroy( destroyCave );
+                return;
+            }
 			other.gameObject.GetComponent<RectTransform>().position = vec3;
 		}
 	}
