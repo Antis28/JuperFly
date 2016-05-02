300277f0fad8c3c6d6f81404da7254afe2cfdebf
Antis Smith <Antis Smith>
2016-05-02 23:02:30 +0300

    Открытие сундука.

---
+++ Assets/1_Scripts/Triggers/ChestOpen.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class ChestOpen : MonoBehaviour
+{
+    public GameObject bonus;
+    public int countBonus = 5;
+    public float forceUp = 1;
+    public bool isKeyUp = true;
+
+
+
+    void OnTriggerEnter2D( Collider2D other )
+    {
+        if( other.tag == "Player"  && isKeyUp )
+        {
+            SpawnBonus();
+        }
+    }
+
+    private void SpawnBonus()
+    {
+
+        StartCoroutine( "DelayBonus");
+        
+    }
+    IEnumerator DelayBonus()
+    {
+        for( var i = 0; i < countBonus; i++ )
+        {
+            GameObject go = (GameObject)Instantiate( bonus, this.transform.position + new Vector3(0,3), Quaternion.identity );
+            Rigidbody2D rBonus = go.GetComponent<Rigidbody2D>();
+            rBonus.AddForce( new Vector2( 0, forceUp * 1000f ) );
+            yield return new WaitForSeconds(0.5f);
+        }
+        Destroy( this.gameObject );
+    }
+}
