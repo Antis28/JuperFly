c729bdd9455bf1daa03ab90177e612a5e703b05c
Antis Smith <Antis Smith>
2016-05-05 09:12:27 +0300

    Переделал установку родителя соответсвенно рекомендациям.

--- Assets/1_Scripts/Bonus_Items/CoinsSpawn.cs
+++ Assets/1_Scripts/Bonus_Items/CoinsSpawn.cs
@@ -1,32 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CoinsSpawn : MonoBehaviour {
+public class CoinsSpawn : MonoBehaviour
+{
 
-	public Transform[] coinSpawns;
+    public Transform[] coinSpawns;
     public int[] num;
-	public GameObject coin;
+    public GameObject coin;
 
-	// Use this for initialization
-	void Start () 
-	{
-		Spawn ();
-	}
-	void Spawn()
-	{
+    // Use this for initialization
+    void Start()
+    {
+        Spawn();
+    }
+    void Spawn()
+    {
         num = new int[coinSpawns.Length];
-        for (int i = 0; i < coinSpawns.Length; i++)
+        for( int i = 0; i < coinSpawns.Length; i++ )
         {
-            int coinFlip = Random.Range(0, 2);
+            int coinFlip = Random.Range( 0, 2 );
             num[i] = coinFlip;
-            if (coinFlip != 0 & coin != null)
+            if( coinFlip != 0 & coin != null )
             {
-                
-                var go = Instantiate(coin, coinSpawns[i].position, Quaternion.identity) as GameObject;
-                go.transform.parent = coinSpawns[i].transform;
+
+                var go = Instantiate( coin, coinSpawns[i].position, Quaternion.identity ) as GameObject;
+                //go.transform.parent = coinSpawns[i].transform;
+                go.transform.SetParent( coinSpawns[i].transform );
 
             }
-		}
-	}
+        }
+    }
 
 }
