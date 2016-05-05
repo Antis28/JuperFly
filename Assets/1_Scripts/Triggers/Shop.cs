aef89fe2e6683ef3a6704aecee828f2700b22741
Antis Smith <Antis Smith>
2016-05-05 09:07:38 +0300

    Optimisation code.

--- Assets/1_Scripts/Triggers/Shop.cs
+++ Assets/1_Scripts/Triggers/Shop.cs
@@ -2,51 +2,60 @@
 using UnityEngine.UI;
 using System.Collections;
 
-public class Shop : MonoBehaviour {
+public class Shop : MonoBehaviour
+{
 
-	public Text shopText;
-	private GameObject shopTextGO;
-	private Canvas TaskCanvas;
-	private Canvas GameCanvas;
-	public SimplePlatformContoroler simplePC;
-	private bool isActive = false;
-	private PoolR poolr;
+    public Text shopText;
+    private GameObject shopTextGO;
+    private Canvas TaskCanvas;
+    private Canvas GameCanvas;
+    public SimplePlatformContoroler simplePC;
+    private bool isActive = false;
 
-	void Start()
-	{
-		poolr = GameObject.Find("PoolReference").GetComponent<PoolR>();
-		simplePC		= poolr.simplePlatformContoroler;
-		GameCanvas		= poolr.GameCanvas;
-		TaskCanvas		= PoolReference.TableScene[EnumInPool.TaskCanvas.ToString()].GetComponent<Canvas>();
-        shopTextGO = poolr.ShopText.gameObject;
-		shopTextGO.SetActive (false); 
-
-
-	}
+    void Start()
+    {
+        if( simplePC == null )
+            simplePC = PoolReference.TableScene[EnumInPool.hero.ToString()].GetComponent<SimplePlatformContoroler>();
+        GameCanvas = PoolReference.TableScene[EnumInPool.GameCanvas.ToString()].GetComponent<Canvas>();
+        TaskCanvas = PoolReference.TableScene[EnumInPool.TaskCanvas.ToString()].GetComponent<Canvas>();
+        shopTextGO = shopText.gameObject;
+        shopTextGO.SetActive( false );
 
 
-	void OnTriggerStay2D(Collider2D other)
-	{
-		if (other.gameObject.CompareTag ("Player"))
-		{
-			shopTextGO.SetActive (true); 
-			poolr.ShopText.text = "Enter to shop \n Press key \" Enter \"";
-			if(Input.GetKeyDown(KeyCode.Return))
-			{
-				simplePC.enabled = false;
-				GameCanvas.gameObject.SetActive (isActive);
-				TaskCanvas.gameObject.SetActive (!isActive);
-//				Debug.Log ("if(Input.GetKey(KeyCode.E))");
-			}
-		}
+    }
 
-	}
-	void OnTriggerExit2D(Collider2D other) {
 
-		if (other.gameObject.CompareTag ("Player"))
-		{
-			shopTextGO.SetActive (false);
-		}
-	}
+    void OnTriggerStay2D( Collider2D other )
+    {
+        if( other.gameObject.CompareTag( "Player" ) )
+        {
+            if( Input.GetKeyDown( KeyCode.Return ) )
+            {
+                simplePC.enabled = false;
+                GameCanvas.gameObject.SetActive( isActive );
+                TaskCanvas.gameObject.SetActive( !isActive );
+                //				Debug.Log ("if(Input.GetKey(KeyCode.E))");
+            }
+        }
+
+    }
+
+    void OnTriggerEnter2D( Collider2D other )
+    {
+        if( other.gameObject.CompareTag( "Player" ) )
+        {
+            shopTextGO.SetActive( true );
+            shopText.text = "Enter to shop \n Press key \" Enter \"";
+        }
+    }
+
+    void OnTriggerExit2D( Collider2D other )
+    {
+
+        if( other.gameObject.CompareTag( "Player" ) )
+        {
+            shopTextGO.SetActive( false );
+        }
+    }
 
 }
